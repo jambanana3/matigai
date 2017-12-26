5d51e9afe592d3897d011e298485a31df8dcdb70
ito <i>
2017-12-26 12:57:56 +0900

    ステージセレクト完成。

--- SceneStageSelect.cpp
+++ SceneStageSelect.cpp
@@ -1,21 +1,48 @@
 #include "SceneStageSelect.h"
+#include "SceneGameMo2.h"
+#include "SceneGameYu.h"
+
+#include "DataScore.h"
+#include "Mouse.h"
+#include "TouchPoint.h"
 
 
-
-SceneStageSelect::SceneStageSelect(){
+SceneStageSelect::SceneStageSelect( ) {
 	setNext(nullptr);
 }
 
 
-SceneStageSelect::~SceneStageSelect()
-{
+SceneStageSelect::~SceneStageSelect( ) {
 }
 
 //\------ いじるのここから下 (   void Scene,,,::run( ) のなか  ) ---------//
 
 void SceneStageSelect::run() {
+	Mouse* mousePtr( new Mouse( ) );
+	
+	TouchPoint* go_game_mo2( new TouchPoint( 100,300 ) );
+	go_game_mo2->setPos( 100, 100 );
+
+	TouchPoint* go_game_yu( new TouchPoint( 100,300 ) );
+	go_game_yu->setPos( 300, 300 );
+
 
 	while (true) {//  ---------　これメインループ　－－－－・・
+		mousePtr->update( );
+
+		DrawFormatString( 100+5,100+5,0xffffff, "ステージ　yu" );
+		go_game_yu->draw;
+		if ( go_game_yu->touch( mousePtr, mousePtr->left_in( ) ) ) {
+			setNext( new SceneGameYu( ) );
+			break;
+		}
+
+		DrawFormatString( 100+5,100+5,0xffffff, "ステージ　mo" );
+		go_game_mo2->draw;
+		if ( go_game_mo2->touch( mousePtr, mousePtr->left_in( ) ) ) {
+			setNext( new SceneGameMo2( ) );
+			break;
+		}
 
 
 
@@ -23,5 +50,8 @@ void SceneStageSelect::run() {
 			break;
 		}
 	}
+	delete mousePtr;
+	delete go_game_mo2;
+	delete go_game_yu;
 
 }
